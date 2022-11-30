d575e23b8a85e79905ca90ff54f00d2a931ec881
thomasblond <103584160+thomasblond@users.noreply.github.com>
2022-11-30 14:23:20 +0100

    Adding client class

--- client.cpp
+++ client.cpp
@@ -1,1 +1,21 @@
+#include "Client.h"
+
+Client::Client(std::string id, std::string firstName, std::string lastName, std::vector<int> shoppingCart): _id(id), _firstName(firstName), _lastName(lastName), _vector<int>(vector<int>){
+}
+
+std::string Client::getId(){
+    return _id;
+}
+
+std::string Client::getFirstName(){
+    return _firstName;
+}
+
+std::string Client::getLastName(){
+    return _lastName;
+}
+
+std::vector<int> Client::getShoppingCart(){
+    return _shoppingCart;
+}
 
--- client.h
+++ client.h
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <vector>
 
+class Client {
+    public:
+        Client (std::string id, std::string firstName, std::string lastName, std::vector<int> shoppingCart);
+        std::string getId();
+        std::string getFirstName();
+        std::string getLastName();
+        std::vector<int> getShoppingCart();
+        bool addItems();
+        bool modifyItems();
+        void removeItem();
+        void cleanShoppingCart();
+
+
+    private:
+        std::string _id; 
+        std::string _firstName;
+        std::string _lastName; 
+        std::vector<int> _shoppingCart;
+};
